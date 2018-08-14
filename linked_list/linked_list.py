d25643445d9761050f2cb2db32db250cf7f1e29f
Lisa Canini <three_nineteen@yahoo.com>
2018-08-13 18:52:22 -0700

    initial commit

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -7,6 +7,7 @@ class Node:
   def __init__(self, value=None, next_node=None):
     self.value = value
     self.next_node = next_node
+    #pointer
 
   def get_value(self):
     return self.value
@@ -23,12 +24,31 @@ class LinkedList:
     self.tail = None
 
   def add_to_tail(self, value):
-    pass
+    new_node = Node(value)
+    if self.head == None:
+      #set head and tail equal to same Node obj
+      self.head = new_node
+      self.tail = new_node
+    else:
+      #set old tails next to new node
+      self.tail.set_next = new_node
+      tail = new_node
 
   def remove_head(self):
-    pass
+    #if head doesn't exist
+    if self.head == None:
+      return None
+    else:
+    #set head.next = head return head.value
+    #if head and tail are same, do something...
 
   def contains(self, value):
+    cur_node = self.head
+    while not cur_node == None:
+      #if found value, return True
+      #update cur_node
+      
+    #else return False  
     pass
 
   def get_max(self):
