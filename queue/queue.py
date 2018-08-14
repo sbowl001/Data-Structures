ec6e7dc947b2f147a36d7e6ebd1a53f4d4fc91ff
Stephanie Bowles <sbowl001@fiu.edu>
2018-08-15 02:06:24 +0800

    queue passing

--- queue/queue.py
+++ queue/queue.py
@@ -8,10 +8,16 @@ class Queue:
     self.storage = LinkedList()
 
   def enqueue(self, item):
-    pass
+    self.storage.add_to_tail(item)
+    self.size += 1
+    
   
   def dequeue(self):
-    pass
+    if self.size == 0: 
+      return None
+    removed = self.storage.remove_head()
+    self.size -= 1 
+    return removed 
 
   def len(self):
-    pass
+    return self.size
