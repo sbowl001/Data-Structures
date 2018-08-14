c7eabe3c10bb5e304258b6cba3cbc57153ded85c
Stephanie Bowles <sbowl001@fiu.edu>
2018-08-15 02:01:53 +0800

    linked_list pass

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -23,13 +23,41 @@ class LinkedList:
     self.tail = None
 
   def add_to_tail(self, value):
-    pass
+    new_node = Node(value)
+    if self.tail == None:
+      self.head = new_node
+      self.tail = new_node 
+    else: 
+      old_tail = self.tail
+      old_tail.set_next(new_node)
+      self.tail = new_node 
 
   def remove_head(self):
-    pass
+    if self.head == None:
+      return None
+    else: 
+      old_head = self.head
+      self.head = old_head.next_node
+    if self.head == None: 
+      self.tail = None
+    return old_head.value 
 
   def contains(self, value):
-    pass
+    cur_node = self.head
+    while not cur_node == None:
+      if value == cur_node.value: 
+        return True
+      else: 
+        cur_node = cur_node.next_node
+    return False
 
   def get_max(self):
-    pass
+    if self.head == None: 
+      return None
+    cur_node = self.head
+    biggest = self.head.value 
+    while not cur_node == None:
+      if biggest < cur_node.value: 
+        biggest = cur_node.value
+      cur_node = cur_node.next_node
+    return biggest 
