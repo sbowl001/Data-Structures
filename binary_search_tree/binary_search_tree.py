e637cf7e6d4c6684ba7b536b6c68b9de8e3cbda0
Stephanie Bowles <sbowl001@fiu.edu>
2018-08-16 01:23:06 +0800

    binary passing

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,10 +5,43 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    pass
+    if self == None: 
+      self = BinarySearchTree( value )
+    else: 
+      cur_node = self
+      done = False
+      while not done:
+        #Left child
+        if cur_node.value >= value: 
+          if not cur_node.left == None:
+            cur_node = cur_node.left
+          else:
+            cur_node.left = BinarySearchTree( value )
+            done = True
+        #Right Child
+        elif cur_node.value < value:
+          if not cur_node.right == None:
+            cur_node = cur_node.right
+          else: 
+            cur_node.right = BinarySearchTree(value)
+            done = True
+ 
 
   def contains(self, target):
-    pass
+    cur_node = self
+    while cur_node: 
+      if target == cur_node.value:
+        return True
+      elif target < cur_node.value:
+        cur_node = cur_node.left
+      else: 
+        cur_node = cur_node.right
+    return False
+
+  
 
   def get_max(self):
-    pass
+    cur_node = self
+    while cur_node.right:
+      cur_node = cur_node.right
+    return cur_node.value
