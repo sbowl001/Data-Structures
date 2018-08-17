77a19b33f2c19c96f72a105d5890c0c2f5cefa9e
Stephanie Bowles <sbowl001@fiu.edu>
2018-08-17 10:17:34 +0800

    heaps passing

--- heap/heap.py
+++ heap/heap.py
@@ -12,17 +12,19 @@ class Heap:
     pass
 
   def delete(self):
-    retval = self.storage[1]
-
-    self.storage[1] = self.storage[self.size]
+    old_root = self.storage[0]
+    new_root = self.storage.pop()
     self.size -= 1
-    self.storage.pop()
-    self._sift_down(1)
-    return retval
+
+    if self.size > 0:
+      self.storage[0] = new_root
+      index = 0
+      self._sift_down(index)
+    return old_root
     
 
   def get_max(self):
-    return self.storage[1]
+    return self.storage[0]
  
 
   def get_size(self):
@@ -30,13 +32,34 @@ class Heap:
   
 
   def _bubble_up(self, index):
-    # while index //2 >0:
-    #   if self.storage[index // 2] <self.storage[index]:
-    #     self.storage[index //2 ], self.storage[index] = self.storage[index], self.storage[index // 2]
+    while index:
+      parent_index = (index -1 ) // 2
+      if self.storage[parent_index] < self.storage[index]:
+        self.storage[parent_index], self.storage[index] = self.storage[index], self.storage[parent_index]
+        index = parent_index
+      else:
+        break
     # if self.storage[index] < self.storage[ (floor(index/2))]:
     #   return 
     # else: 
     pass
 
   def _sift_down(self, index):
+    while index < self.get_size():
+      child_index1 = index *2 + 1
+      child_index2 = index *2 + 2
+      if child_index1 >= self.get_size():
+        break
+      elif child_index2 >= self.get_size():
+        larger_child_index = child_index1
+      elif self.storage[child_index1] > self.storage[child_index2]:
+         larger_child_index = child_index1
+      else: 
+        larger_child_index = child_index2
+
+      if self.storage[index] < self.storage[larger_child_index]:
+        self.storage[index], self.storage[larger_child_index] = self.storage[larger_child_index], self.storage[index]
+        index = larger_child_index
+      else: 
+        break
     pass
